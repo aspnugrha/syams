02d4f0bf713c4eed72d4cd4af16088d15c75c5f0
Ad Skuy <aspnugrha@gmail.com>
2025-12-10 09:08:29 +0700

    perbaikan header email

--- resources/views/frontend/email/forgot-password.blade.php
+++ resources/views/frontend/email/forgot-password.blade.php
@@ -1,6 +1,6 @@
 @extends('frontend.email.layouts.email-app')
 @section('content')
-    <div class="header">
+    <div class="header" style="padding: 25px;margin-bottom: 20px;">
         <a class="text-decoration-none" href="{{ route('home') }}">
             <img src="{{ asset($company_profile && $company_profile->logo ? 'assets/image/upload/logo/'.$company_profile->logo : 'assets/image/logo-syams.png') }}" alt="{{ ($company_profile && $company_profile->name ? $company_profile->name : 'Syams Manufacturing') }} Logo" style="width: 140px;">
         </a>
